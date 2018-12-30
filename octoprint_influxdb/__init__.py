f7c2c4b51cd146a606f6d6dbe2cf0c8e16f83a99
Aaron Griffith <aargri@gmail.com>
2018-12-29 23:16:53 -0500

    initial code to connect and push data in to influx

--- octoprint_influxdb/__init__.py
+++ octoprint_influxdb/__init__.py
@@ -1,9 +1,13 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import platform
+import datetime
+
 import octoprint.plugin
 import octoprint.util
 import influxdb
+import requests.exceptions
 
 __plugin_name__ = "InfluxDB Plugin"
 
@@ -24,6 +28,45 @@ class InfluxDBPlugin(octoprint.plugin.SettingsPlugin,
 
 	def __init__(self):
 		self.influx_timer = None
+		self.influx_db = None
+		self.influx_kwargs = None
+		self.influx_common_tags = {
+			'host': platform.node(),
+		}
+
+	def influx_flash_exception(self, message):
+		self._logger.exception(message)
+		# FIXME flash something to the user, probably needs JS
+
+	def influx_try_connect(self, kwargs):
+		# create a safe copy we can dump out to the log, modify fields
+		kwargs = kwargs.copy()
+		kwargs_safe = kwargs.copy()
+		for k in ['username', 'password']:
+			if k in kwargs_safe:
+				del kwargs_safe[k]
+		kwargs_log = ", ".join("{}={!r}".format(*v) for v in sorted(kwargs_safe.items()))
+		self._logger.info("connecting: {}".format(kwargs_log))
+
+		dbname = 'octoprint'
+		if 'database' in kwargs:
+			dbname = kwargs.pop('database')
+
+		try:
+			db = influxdb.InfluxDBClient(**kwargs)
+			db.ping()
+		except Exception:
+			# something went wrong connecting :(
+			self.influx_flash_exception('Cannot connect to InfluxDB server.')
+			return None
+		try:
+			db.create_database(dbname)
+			db.switch_database(dbname)
+		except Exception:
+			# something went wrong making the database
+			self.influx_flash_exception('Cannot create InfluxDB database.')
+			return None
+		return db
 
 	def influx_reconnect(self):
 		# stop the old timer, if we need to
@@ -43,8 +86,7 @@ class InfluxDBPlugin(octoprint.plugin.SettingsPlugin,
 		if self._settings.get_boolean(['authenticate']):
 			add_arg_if_exists('username', ['username'])
 			add_arg_if_exists('password', ['password'])
-		# save database for after connection, in case
-		# it doesn't exist
+		add_arg_if_exists('database', ['database'])
 		kwargs['ssl'] = self._settings.get_boolean(['ssl'])
 		if kwargs['ssl']:
 			kwargs['verify_ssl'] = self._settings.get_boolean(['verify_ssl'])
@@ -53,25 +95,56 @@ class InfluxDBPlugin(octoprint.plugin.SettingsPlugin,
 			kwargs['udp_port'] = kwargs['port']
 			del kwargs['port']
 
-		# create a safe copy we can dump out to the log
-		kwargs_safe = kwargs.copy()
-		for k in ['username', 'password']:
-			if k in kwargs_safe:
-				del kwargs_safe[k]
-		kwargs_log = ", ".join("{}={!r}".format(*v) for v in sorted(kwargs_safe.items()))
-		self._logger.info("InfluxDB reconnecting: {}".format(kwargs_log))
-
-		# perform reconnection, if we need to
+		if self.influx_db is None or kwargs != self.influx_kwargs:
+			self.influx_db = self.influx_try_connect(kwargs)
+			self.influx_kwargs = kwargs
 
 		# start a new timer
-		interval = self._settings.get_float(['interval'], min=0)
-		if not interval:
-			interval = self.get_settings_defaults()['interval']
-		self.influx_timer = octoprint.util.RepeatedTimer(interval, self.influx_gather)
-		self.influx_timer.start()
+		if self.influx_db:
+			interval = self._settings.get_float(['interval'], min=0)
+			if not interval:
+				interval = self.get_settings_defaults()['interval']
+			self.influx_timer = octoprint.util.RepeatedTimer(interval, self.influx_gather)
+			self.influx_timer.start()
+
+	def influx_emit(self, measurement, fields, extra_tags={}):
+		tags = self.influx_common_tags.copy()
+		tags.update(extra_tags)
+		# python doesn't put the Z at the end
+		# because python cannot into timezones until Python 3
+		time = datetime.datetime.utcnow().isoformat() + 'Z'
+		point = {
+			'measurement': measurement,
+			'tags': tags,
+			'time': time,
+			'fields': fields,
+		}
+		try:
+			self.influx_db.write_points([point])
+		except Exception:
+			# we were dropped! try to reconnect
+			self.influx_flash_exception("Disconnected from InfluxDB. Attempting to reconnect.")
+			self.influx_db = None
+			self.influx_reconnect()
 
 	def influx_gather(self):
-		self._logger.info("Gathering...")
+		# if we're not connected to a database, do nothing
+		if not self.influx_db:
+			return
+		# if we're not connected to a printer, do nothing
+		if not self._printer.is_operational():
+			return
+
+		temps = self._printer.get_current_temperatures()
+		if not temps:
+			return
+
+		fields = {}
+		for sensor in temps:
+			for subfield in temps[sensor]:
+				fields[sensor + '_' + subfield] = temps[sensor][subfield]
+
+		self.influx_emit('temperature', fields)
 
 	##~~ StartupPlugin mixin
 
